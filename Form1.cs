a8ce27981d385b4caf62299f992ccb3ace13c212
Aidar <sharipovaydar200515@gmail.com>
2024-04-28 11:45:54 +0600

    Внедряю свой obj парсер сюда

--- Form1.cs
+++ Form1.cs
@@ -64,17 +64,17 @@ namespace WindowsFormsTest
 
             // Куб /////////////////////////////
             // Сбрасываем модельно-видовую матрицу
-            gl.LoadIdentity();
+            //gl.LoadIdentity();
             // Сдвигаем перо с право от центра и вглубь экрана
-            gl.Translate(2.5f, 0.0f, -10.0f);
+            //gl.Translate(2.5f, 0.0f, -10.0f);
             // Вращаем куб вокруг ее оси Y
-            gl.Rotate(rtri, 0.0f, 1.5, 0.0f);
+            //gl.Rotate(rtri, 0.0f, 1.5, 0.0f);
             // Рисуем грани куба
-            gl.Begin(OpenGL.GL_QUADS);
+            //gl.Begin(OpenGL.GL_QUADS);
 
-            drawCube();
+            //drawCube();
 
-            gl.End();
+            //gl.End();
 
             // Контроль полной отрисовки следующего изображения
             gl.Flush();
@@ -108,19 +108,19 @@ namespace WindowsFormsTest
             gl.Color(0.0f, 0.0f, 1.0f);
             gl.Vertex(1.0f, -1.0f, 1.0f);
             // Back
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            //gl.Color(1.0f, 0.0f, 0.0f);
+            //gl.Vertex(0.0f, 1.0f, 0.0f);
+            //gl.Color(0.0f, 1.0f, 0.0f);
+            //gl.Vertex(1.0f, -1.0f, -1.0f);
+            //gl.Color(0.0f, 0.0f, 1.0f);
+            //gl.Vertex(-1.0f, -1.0f, -1.0f);
             // Left
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            //gl.Color(1.0f, 0.0f, 0.0f);
+            //gl.vertex(0.0f, 1.0f, 0.0f);
+            //gl.color(0.0f, 1.0f, 0.0f);
+            //gl.vertex(-1.0f, -1.0f, 1.0f);
+            //gl.color(0.0f, 0.0f, 1.0f);
+            //gl.vertex(-1.0f, -1.0f, -1.0f);
 
         }
 
@@ -207,4 +207,85 @@ namespace WindowsFormsTest
             return base.ProcessCmdKey(ref msg, keyData);
         }
     }
+
+    // Класс парсер
+    // В метод Parse(filePath) дается путь к .obj файлу. Он его парсит и извлекает линии в отдельные списки в зависимости от токена в начале линии(v/vt/vn/f)
+    // Списки не возвращаются, а храняться статически как свойства класса
+    class ObjParser
+    {
+        // Координаты всех вершин треугольников
+        public static List<string> vertexes = new List<string>();
+
+        // Координаты текстур
+        public static List<string> textures = new List<string>();
+
+        // Нормали
+        public static List<string> normals = new List<string>();
+
+        // Лица треугольников
+        public static List<string> faces = new List<string>();
+
+        // Возвращает список где каждая линия файла это элемент списка
+        private static string[] GetAllText(string filePath)
+        {
+            // Каждая строка файла отдельный элемент в массиве
+            string[] linesArray = File.ReadAllLines(filePath);
+
+            return linesArray;
+        }
+
+        // Возвращает новую строку без токена в начале линии. То есть остается только значение
+        public static string RemoveToken(string line)
+        {
+            string[] words = line.Split();
+            string result = string.Join(" ", words.Skip(1));
+
+            return result;
+        }
+
+        // Сортирует линии по отдельным спискам. Вершины в одном списке, нормали в одной списке и т.д
+        private static void SortLines(string[] lines)
+        {
+            // Проверяeт каждую линию и добавляет ее в соответстующий список в зависимости от токена(v/vt и т.д)
+            foreach (string line in lines)
+            {
+                // Вершина 
+                if (line[0] == 'v' && line[1] == ' ')
+                {
+                    vertexes.Add(RemoveToken(line));
+                }
+                // Текстура
+                else if (line[0] == 'v' && line[1] == 't')
+                {
+                    textures.Add(RemoveToken(line));
+                }
+                // Нормаль
+                else if (line[0] == 'v' && line[1] == 'n')
+                {
+                    normals.Add(RemoveToken(line));
+                }
+                // Лицо
+                else if (line[0] == 'f')
+                {
+                    faces.Add(RemoveToken(line));
+                }
+            }
+        }
+
+        // Это будет финальной функций которая будет делать все сразу.
+        // Парсит файл, вытаскивает все нужные данные и организовывает их формате, который понимают движки для 3д рендеринга
+        public static void Parse(string filePath)
+        {
+            // Получаем список со всеми линиями текста
+            string[] lines = GetAllText(filePath);
+
+            // Запихиваем из по спискам
+            SortLines(lines);
+
+            // TODO
+            // Огранизует в тип данных который понятен для движков 3д рендеринга. Лица со своими настоящими значениями, а не индексы
+            ReplaceFaceValues();
+        }
+    }
+
 }
