e8239797eb5ee5b29bdffdaec174c0010f6afb3d
Aidar <sharipovaydar200515@gmail.com>
2024-04-28 23:25:47 +0600

    фыва

--- Form1.cs
+++ Form1.cs
@@ -21,11 +21,7 @@ namespace WindowsFormsTest
         
         public Form1()
         {
-        
             InitializeComponent();
-
-            // Subscribe to the KeyDown event
-            KeyPreview = true;
 
             // Создаем экземпляр
             gl = new OpenGL();
@@ -43,43 +39,35 @@ namespace WindowsFormsTest
 
         /// Угол поворота пирамиды
         float rtri = 0;
-        string direction = "right";
 
         // Путь к файлу
         string filePath = "../../sword/sword.obj";
 
+        // Много раз в секунду вызывается эта функция
         private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+
+        }
+
+        // То что происходит при инициализации glControl 
+        private void openGLControl1_Load(object sender, EventArgs e)
         {
             // Очистка экрана и буфера глубин
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
 
-            // Пирамида /////////////////////////////
-            // Сбрасываем модельно-видовую матрицу
-            gl.LoadIdentity();
-            // Сдвигаем перо влево от центра и вглубь экрана
-            gl.Translate(-2.5f, 0.0f, -10.0f);
-            // Вращаем пирамиду вокруг ее оси Y
-            gl.Rotate(rtri, 0.0f, 1.0f, 0.0f);
-            // Рисуем треугольники - грани пирйамиды
-            gl.Begin(OpenGL.GL_TRIANGLES);
-
-            drawTriangle();
-
-            gl.End();
-
             // Куб /////////////////////////////
             // Сбрасываем модельно-видовую матрицу
-            //gl.LoadIdentity();
+            gl.LoadIdentity();
             // Сдвигаем перо с право от центра и вглубь экрана
-            //gl.Translate(2.5f, 0.0f, -10.0f);
+            gl.Translate(2.5f, 0.0f, -10.0f);
             // Вращаем куб вокруг ее оси Y
-            //gl.Rotate(rtri, 0.0f, 1.5, 0.0f);
+            gl.Rotate(rtri, 0.0f, 1.5, 0.0f);
             // Рисуем грани куба
-            //gl.Begin(OpenGL.GL_QUADS);
+            gl.Begin(OpenGL.GL_QUADS);
 
-            //drawCube();
+            drawCube();
 
-            //gl.End();
+            gl.End();
 
             // Оbj файл /////////////////////////////
             // Сбрасываем модельно-видовую матрицу
@@ -97,56 +85,14 @@ namespace WindowsFormsTest
 
             // Контроль полной отрисовки следующего изображения
             gl.Flush();
-
-            // Меняем в какую сторону крутиться обЪект, меняя угол поворота 
-            if (direction == "right")
-            {
-                rtri += 10.0f;
-            } 
-            else if (direction == "left")
-            {
-                rtri -= 10.0f;
-            }
         }
+
 
         // Рисует obj файл
         private void drawObj(string filepath)
         {
             // парсит
             ObjParser.Parse(filepath);   
-        }
-
-        // Рисует триугольник
-        private void drawTriangle()
-        {
-            // Front
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(1.0f, -1.0f, 1.0f);
-            // Right
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(1.0f, -1.0f, 1.0f);
-            // Back
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(1.0f, -1.0f, -1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
-            // Left
-            gl.Color(1.0f, 0.0f, 0.0f);
-            gl.Vertex(0.0f, 1.0f, 0.0f);
-            gl.Color(0.0f, 1.0f, 0.0f);
-            gl.Vertex(-1.0f, -1.0f, 1.0f);
-            gl.Color(0.0f, 0.0f, 1.0f);
-            gl.Vertex(-1.0f, -1.0f, -1.0f);
         }
 
         // Рисует куб
@@ -191,45 +137,10 @@ namespace WindowsFormsTest
 
         }
 
-        // То что происходит при инициализации glControl 
-        private void openGLControl1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         // При нажатии на текст в окошке
         private void label1_Click(object sender, EventArgs e)
         {
 
-        }
-
-        // При нажатии на клавиши со стрелками
-        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
-        {
-            if (keyData == Keys.Left)
-            {
-                label1.Text = "Left arrow key pressed";
-                direction = "left";
-                return true;
-            }
-            else if (keyData == Keys.Right)
-            {
-                label1.Text = "Rigt arrow key pressed";
-                direction = "right";
-                return true;
-            }
-            else if (keyData == Keys.Up)
-            {
-                label1.Text = "Up arrow key pressed";
-                return true;
-            }
-            else if (keyData == Keys.Down)
-            {
-                label1.Text = "Down arrow key pressed";
-                return true;
-            }
-
-            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 
@@ -238,7 +149,7 @@ namespace WindowsFormsTest
     // Списки не возвращаются, а храняться статически как свойства класса
     class ObjParser
     {
-        // Координаты всех вершин треугольников
+        // Координаты всех вершин треугольников. Каждый элемент это отдельный список, где элементы это координаты по x, y, z
         public static List<List<float>> vertexes = new List<List<float>>();
 
         // Координаты текстур
